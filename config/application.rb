689927e2e0bb6159d828808c8a9fed99bc91b3a9
napple29 <snwxxx29@gmail.com>
2021-08-18 16:16:22 +0900

    提出、日本語・英語対応

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,10 @@ module BooksApp
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+      config.active_record.default_timezone = :local
+      config.i18n.default_locale = :ja
+      config.i18n.available_locales = %i(ja en)
+      config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
