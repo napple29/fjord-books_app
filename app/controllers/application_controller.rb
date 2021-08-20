041ef68d8e7890f5bab6c7efc5e53c76987ef173
napple29 <snwxxx29@gmail.com>
2021-08-20 16:52:56 +0900

    ログイン、ログアウト後のルーティングを変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,11 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    "/user/#{current_user.id}"
+    "/books"
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    "/users/sign_in"
   end
 
   protected
