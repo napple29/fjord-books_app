794631ca5c9686b31bfee175294644c56f804a41
napple29 <snwxxx29@gmail.com>
2021-09-04 00:20:07 +0900

    ユーザー情報編集後、ユーザー一覧画面に戻るようルーティング、不要なファイルの削除

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -63,4 +63,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+    def after_update_path_for(resource)
+      users_index_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  devise_for :users
   resources :books
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users ,:controllers => { :registrations => 'users/registrations' }
+  get 'users/index'
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
