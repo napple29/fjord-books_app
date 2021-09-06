e4e55bbc50639118907c3df9e8b87acdbd3f57ce
napple29 <snwxxx29@gmail.com>
2021-09-06 23:29:04 +0900

    ユーザー一覧から詳細画面を表示するよう変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :books
   devise_for :users ,:controllers => { :registrations => 'users/registrations' }
   get 'users/index'
+  get 'users/show'
+  #get 'users/show/#{current_user.id}'
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
