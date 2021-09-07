2dcef524fb9cb824500e61d2a235821cf34562db
napple29 <snwxxx29@gmail.com>
2021-09-07 15:49:17 +0900

    各ユーザーの詳細を表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :books
   devise_for :users ,:controllers => { :registrations => 'users/registrations' }
   get 'users/index'
-  get 'users/show'
-  #get 'users/show/#{current_user.id}'
+  get 'users/:id', to: 'users#show', as: 'users_show'
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
