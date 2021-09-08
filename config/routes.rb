7518fc5216a0fe2463aa3b392915f412142d5c4d
napple29 <snwxxx29@gmail.com>
2021-09-08 23:29:45 +0900

    letter_opener_webを使用してパスワード再設定を可能にした

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users ,:controllers => { :registrations => 'users/registrations' }
   get 'users/index'
   get 'users/:id', to: 'users#show', as: 'users_show'
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
