0476dd48bd1db431ef2d84bd3bbb342cbf46a805
napple29 <snwxxx29@gmail.com>
2021-09-24 13:05:46 +0900

    詳細ページにフォローフォロワーの統計を表示、名前の区別が付きづらいため関係を表すfollower,followingsの名前を変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,21 +6,21 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
 
-  has_many :followings, class_name: "Relationship",
+  has_many :user_followings, class_name: "Relationship",
             foreign_key: "follower_id",
             dependent: :destroy
-  has_many :followers, class_name: "Relationship",
+  has_many :user_followers, class_name: "Relationship",
             foreign_key: "following_id",
             dependent: :destroy
-  has_many :following, through: :followings, source: :following
-  has_many :followers, through: :followers, source: :follower
+  has_many :following, through: :user_followings, source: :following
+  has_many :followers, through: :user_followers, source: :follower
 
   def follow(other_user)
-    followings.create(following_id: other_user.id)
+    user_followings.create(following_id: other_user.id)
   end
 
   def unfollow(other_user)
-    followings.find_by(following_id: other_user.id).destroy
+    user_followings.find_by(following_id: other_user.id).destroy
   end
 
   def following?(other_user)
