58a72a13610a78d91f6e7f04a65f7a19f7ce3a92
napple29 <snwxxx29@gmail.com>
2021-09-28 17:42:42 +0900

    reportsのviewファイルの作成、reportsの登録から詳細画面への表示まで

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,2 +1,25 @@
 class ReportsController < ApplicationController
+  before_action :set_report, only: %i[show edit update destroy]
+  def new
+    @report = Report.new
+  end
+
+  def create
+    @report = Report.create(report_params)
+    redirect_to report_path(@report.id)
+  end
+
+  def show
+    @report = Report.find(params[:id])
+  end
+  private
+
+  def set_report
+    @report = Report.find(params[:id])
+  end
+
+  def report_params
+    params.require(:report).permit(:id, :title, :content)
+  end
 end
+
