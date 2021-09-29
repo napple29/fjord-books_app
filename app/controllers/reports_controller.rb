2920a8448707ae807b39663f94518756c714d711
napple29 <snwxxx29@gmail.com>
2021-09-29 12:34:41 +0900

    reportsの編集、削除、一覧表示を追加

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,9 @@
 class ReportsController < ApplicationController
   before_action :set_report, only: %i[show edit update destroy]
+  def index
+    @reports = Report.order(:id).page(params[:page])
+  end
+
   def new
     @report = Report.new
   end
@@ -12,6 +16,17 @@ class ReportsController < ApplicationController
   def show
     @report = Report.find(params[:id])
   end
+  
+  def update
+    @report.update(report_params)
+    redirect_to report_path(@report.id)
+  end
+
+  def destroy
+    @report.destroy
+    redirect_to reports_path
+  end
+
   private
 
   def set_report
