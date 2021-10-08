803a312b525c1a112023e26eaa8b677c56ab2763
napple29 <snwxxx29@gmail.com>
2021-10-08 13:24:36 +0900

    create_table のmigrationを修正

--- db/migrate/20210914135155_create_relationships.rb
+++ db/migrate/20210914135155_create_relationships.rb
@@ -4,12 +4,12 @@ class CreateRelationships < ActiveRecord::Migration[6.1]
   def change
     create_table :relationships do |t|
       t.integer :follower_id
-      t.integer :followed_id
+      t.integer :following_id
 
       t.timestamps
     end
     add_index :relationships, :follower_id
-    add_index :relationships, :followed_id
-    add_index :relationships, %i[follower_id followed_id], unique: true
+    add_index :relationships, :following_id
+    add_index :relationships, %i[follower_id following_id], unique: true
   end
 end
--- db/migrate/20210922021409_rename_followed_id_column_to_following_id.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class RenameFollowedIdColumnToFollowingId < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :relationships, :followed_id, :following_id
-  end
-end
