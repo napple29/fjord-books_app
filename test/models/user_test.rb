14d5d25f53d999ba58e58d983054b9e788d19628
napple29 <snwxxx29@gmail.com>
2021-10-13 20:49:21 +0900

    Userモデルのテストを作成

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,20 +3,34 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+
+  test '#following?' do
+    users(:Alice).follow(users(:Bob))
+    assert users(:Alice).following?(users(:Bob))
+  end
+
+  test '#followed_by?' do
+    users(:Alice).follow(users(:Bob))
+    assert users(:Bob).followed_by?(users(:Alice))
+  end
+
+  test '#follow' do
+    assert_not users(:Alice).following?(users(:Bob))
+    users(:Alice).follow(users(:Bob))
+    assert users(:Alice).following?(users(:Bob))
+  end
+
+  test '#unfollow' do
+    users(:Alice).follow(users(:Bob))
+    users(:Alice).unfollow(users(:Bob))
+    assert_not users(:Alice).following?(users(:Bob))
+  end
+
   test "#name_or_email" do
     user = User.new(email: 'foo@example.com', name: '')
     assert_equal 'foo@example.com', user.name_or_email
 
     user.name = 'Foo Bar'
     assert_equal 'Foo Bar', user.name_or_email
-  end
-
-  test '#follow' do
-    me = User.create!(email: 'me@example.com', password: 'password')
-    she = User.create!(email: 'she@example.com', password: 'password')
-
-    assert_not me.following?(she)
-    me.follow(she)
-    assert me.following?(she)
   end
 end
