a31ccdd84d2469d770dcb753b304e4b62f293a2c
napple29 <snwxxx29@gmail.com>
2021-10-13 12:17:54 +0900

    提出前に修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
-  before_action :set_commentable
+  before_action :set_commentable, only: %i[create]
 
   def create
     @comment = @commentable.comments.new(comment_params)
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,14 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, polymorphic: true
+
+  def self.comment_user_name(comment)
+    if comment.user.nil?
+      '退会したユーザ'
+    elsif comment.user.name.blank?
+      comment.user.email
+    else
+      comment.user.name
+    end
+  end
 end
