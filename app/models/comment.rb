f26ac72749868f78eca8398d858272468f486b33
napple29 <snwxxx29@gmail.com>
2021-10-18 12:47:21 +0900

    コメントしたユーザの表示に関する変更

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,10 +4,8 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, polymorphic: true
 
-  def comment_user_name(comment)
-    if comment.user.nil?
-      I18n.t('comment.withdrawal_user')
-    elsif comment.user.name.blank?
+  def name_or_email(comment)
+    if comment.user.name.blank?
       comment.user.email
     else
       comment.user.name
