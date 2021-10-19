ac5b82efda61477b0e2e68dae171cce6f8513627
napple29 <snwxxx29@gmail.com>
2021-10-19 12:55:02 +0900

    連番にしていたreportの名前を変更

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -5,16 +5,16 @@ require 'test_helper'
 class ReportTest < ActiveSupport::TestCase
   test '#editable?' do
     alice = users(:alice)
-    report1 = alice.reports.new
-    assert report1.editable?(users(:alice))
+    alice_report = alice.reports.new
+    assert alice_report.editable?(users(:alice))
 
     bob = users(:bob)
-    report2 = bob.reports.new
-    assert_not report2.editable?(users(:alice))
+    bob_report = bob.reports.new
+    assert_not bob_report.editable?(users(:alice))
   end
 
   test '#created_on' do
-    report1 = users(:alice).reports.new(created_at: '2021-10-14 02:10:11.678236 UTC')
-    assert_equal report1.created_on.to_s, '2021-10-14'
+    alice_report = users(:alice).reports.new(created_at: '2021-10-14 02:10:11.678236 UTC')
+    assert_equal alice_report.created_on.to_s, '2021-10-14'
   end
 end
--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -4,7 +4,7 @@ require 'application_system_test_case'
 
 class ReportsTest < ApplicationSystemTestCase
   setup do
-    @report = reports(:report1)
+    @report = reports(:alice_report)
 
     visit root_url
     fill_in 'Eメール', with: 'alice@example.com'
