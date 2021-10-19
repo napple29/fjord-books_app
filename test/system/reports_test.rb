06bf9b02c1f363cdb54acade27a74fbb66662f83
napple29 <snwxxx29@gmail.com>
2021-10-19 13:48:33 +0900

    システムテストにて、日報を作成・更新された際に正しく表示されているか検証されるよう修正

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -17,10 +17,15 @@ class ReportsTest < ApplicationSystemTestCase
     click_on '日報'
     click_on '新規作成'
 
-    fill_in 'タイトル', with: @report.title
-    fill_in '内容', with: @report.content
+    fill_in 'タイトル', with: 'lsコマンド3日目'
+    fill_in '内容', with: 'ファイルを表示できた！'
     click_on '登録する'
     assert_text '日報が作成されました'
+
+    assert_text 'タイトル'
+    assert_text 'lsコマンド3日目'
+    assert_text '内容'
+    assert_text 'ファイルを表示できた！'
   end
 
   test 'editing the report' do
@@ -31,6 +36,11 @@ class ReportsTest < ApplicationSystemTestCase
     fill_in '内容', with: '３列表示難しい'
     click_on '更新する'
     assert_text '日報が更新されました'
+
+    assert_text 'タイトル'
+    assert_text 'lsコマンド4日目'
+    assert_text '内容'
+    assert_text '３列表示難しい'
   end
 
   test 'destroying the report' do
