9061e2181626d1968205b12019dab4de1b509df8
napple29 <snwxxx29@gmail.com>
2021-10-20 10:14:56 +0900

    レビュー後の修正

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,6 +3,5 @@
 class Relationship < ApplicationRecord
   belongs_to :follower, class_name: 'User'
   belongs_to :following, class_name: 'User'
-  validates :follower_id, uniqueness: { scope: :following_id }
   validates :following_id, uniqueness: { scope: :follower_id }
 end
